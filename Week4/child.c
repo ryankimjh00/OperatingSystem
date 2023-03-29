b9f8bff4399e4774cb0453826950535cc49bf1da
ryankimjh00 <ryankimjh546@gmail.com>
2023-03-29 20:05:38 +0900

    first commit

---
+++ Week4/child.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("I am  a child\n");
+
+    return 100;
+}
---
+++ Week4/pinfo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void)
+{
+        pid_t pid, ppid;
+
+        pid = /* 현재 프로세스의 id */;
+        ppid = /* 현재 프로세스의 부모 프로세스 id */;
+
+        printf("process ID = %d, parent PID = %d\n", pid, ppid);
+
+        return 0;
+}
