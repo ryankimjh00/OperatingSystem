979d009f98f34305eede6320817ec8cc3c83a0ae
ryankimjh00 <ryankimjh546@gmail.com>
2023-03-29 20:32:07 +0900

    week4 act

---
+++ 03/execex.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void)
+{
+	pid_t pid;
+	pid = /* 자식프로세스 생성 */
+
+	if (pid > 0) { // 부모 프로세스 코드
+		printf("부모프로세스: fork()의 리턴 값 = 자식프로세스 pid = %d\n", pid);
+		printf("부모프로세스: 프로세스 pid = %d\n", getpid());
+
+		wait(NULL); // 자식프로세스가 종료할 때까지 대기
+		return 0;
+	}
+	else if (pid == 0) { // 자식 프로세스 코드
+		printf("자식프로세스: fork()의 리턴 값 pid = %d\n", pid);
+		printf("자식프로세스: pid = %d, 부모프로세스 pid = %d\n", getpid(), getppid());
+
+		/* /bin/ls를 현재프로세스에 오버레이하여 실행 */
+		//execlp("ls", "ls", "-l", NULL);   // execl 하면 "ls" 대신에 /bin/ls 를 넣어줘야함
+		char* av[3] = {"ls", "-l", NULL};
+		execv("/bin/ls",av);
+		// 여기부터는 무시됨
+		printf("\n>> 오류: 프로그램 실행 취소!!\n");
+	}
+	else { // fork() 오류
+		printf("fork 오류");
+		return -1;
+	}
+}
---
+++ 03/waitex.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void)
+{
+    pid_t pid;
+    int status;
+
+    pid = fork(); // 자식프로세스 생성
+
+    if (pid > 0) { // 부모 프로세스 코드
+        printf("부모프로세스: 자식의 종료를 기다림\n");
+
+        /* 자식프로세스 종료 대기. status에 종료 코드 받음 */
+	wait(&status);   // status >> 8비트만큼 이동(응답 코드가 32비트중 끝에 있지 않을 경우땜에)
+
+        printf("부모프로세스: child의 종료 코드=%d\n", WEXITSTATUS(status));
+        return 0;
+    }
+    else if (pid == 0) { // 자식 프로세스 코드
+        /* child를 자식프로세스로 실행 */
+	execl("./child", "child", NULL);
+	printf("failed");
+    }
+    else { // fork() 오류
+        printf("fork 오류");
+        return -1;
+    }
+}
