6ae72078e7bf23f0bc5e5e8184ec8b6364a78e05
ryankimjh00 <ryankimjh546@gmail.com>
2023-03-31 18:31:13 +0900

    4주차 과제 끝

--- 03/forkex.c
+++ 03/forkex.c
@@ -7,14 +7,13 @@ int main(void)
 {
     pid_t pid;
     int i, sum = 0;
-    int start, end = 0;
 
     pid = /* 자식프로세스 생성 */fork();
 
     if ( /* 부모 프로세스이면 */pid > 0 ) {
         printf("부모프로세스: fork()의 리턴 값 = 자식프로세스 pid = %d\n", pid);
         printf("부모프로세스: pid = %d\n", getpid());
-        printf("부모 프로세스: 시작값 =%d, 끝값=%d 을 자식 프로세스에 전달\n", start, end);
+
         /* 자식 프로세스가 종료할 때까지 대기 */wait(NULL);
         printf("부모프로세스종료\n");
     }
@@ -22,7 +21,7 @@ int main(void)
         printf("자식프로세스: fork()의 리턴 값 pid = %d\n", pid);
         printf("자식프로세스: pid = %d, 부모프로세스 pid = %d\n", getpid(), getppid());
 
-        for (i = start; i <= end; i++)
+        for (i = 1; i <= 100; i++)
             sum += i;
         printf("자식프로세스: sum = %d\n", sum);
     }
--- Week4/child.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("I am  a child\n");
-
-    return 100;
-}
